598ec020ea4d04b924f6a33a97243fd3a48a4719
Ilya Zvoznikov <ilya.zvoznikov@gmail.com>
2019-03-26 00:29:00 +0700

    create main app logic

--- README.md
+++ README.md
@@ -1,6 +1,28 @@
 # Image Resizer
 
-[TODO. There will be project description]
+The app "Image Resizer" is intended to change height and width of your image.
+
+# How to Install
+
+Python 3 should be already installed. Then use pip (or pip3 if there is a conflict with old Python 2 setup) to install dependencies:
+
+```bash
+$ pip install -r requirements.txt # alternatively try pip3
+```
+
+#Quickstart
+
+The path to the original image file is specified when running after the script name.
+Also you may specify height and (or) width of result image or scale.
+Do not use scale and height (width) simultaneously.
+If path to the result image is not specified the result image will be saved to the same path.
+
+```bash
+$ python image_resize.py google-logo.png --scale=2
+# result - google-logo_1280x688.png in the same path
+```
+
+To see help use option '--help' or '-h'.
 
 # Project Goals
 
--- image_resize.py
+++ image_resize.py
@@ -1,6 +1,7 @@
 import argparse
 from PIL import Image
 import os
+import sys
 
 
 def get_args():
@@ -8,13 +9,13 @@ def get_args():
         description='Do not use --width/--height and --scale simultaneously')
 
     parser.add_argument(
-        '--input', required=True, help='path to original image')
+        'input', help='path to original image')
     parser.add_argument(
         '--output', help='path to result image')
     parser.add_argument(
-        '--height', type=int, help='result image height')
-    parser.add_argument(
         '--width', type=int, help='result image width')
+    parser.add_argument(
+        '--height', type=int, help='result image height')
     parser.add_argument(
         '--scale', type=float, help='result and original image ratio')
 
@@ -29,17 +30,71 @@ def get_args():
     args_dict = {'height': args.height,
                  'width': args.width,
                  'scale': args.scale,
-                 'output_path': args.output,
-                 'input_path': args.input,
+                 'output': args.output,
+                 'input': args.input,
                  }
 
     return args_dict
 
 
-def resize_image(path_to_original, path_to_result):
-    pass
+def resize_image(original_image, height, width, scale):
+    original_image_ratio = original_image.height / original_image.width
+
+    if scale:
+        result_height = int(original_image.height * scale)
+        result_width = int(original_image.width * scale)
+    elif width and height:
+        result_width = width
+        result_height = height
+    elif width:
+        result_width = width
+        result_height = int(width * original_image_ratio)
+    elif height:
+        result_height = height
+        result_width = int(height / original_image_ratio)
+    else:
+        result_width = original_image.width
+        result_height = original_image.height
+
+    result_image = original_image.resize((result_width, result_height))
+    return result_image
+
+
+def get_path_to_result(original_image, result_image):
+    original_image_basename, original_image_ext = os.path.splitext(
+        original_image.filename
+    )
+    result_image_basename = original_image_basename + '_{}x{}'.format(
+        result_image.width, result_image.height
+    )
+    return result_image_basename + original_image_ext
 
 
 if __name__ == '__main__':
     args = get_args()
-    print(args)
+    try:
+        original_image = Image.open(args['input'])
+    except IOError:
+        sys.exit('Incorrect image file')
+
+    result_image = resize_image(
+        original_image=original_image,
+        height=args['height'],
+        width=args['width'],
+        scale=args['scale'],
+    )
+    path_to_result = args['output']
+
+    if not path_to_result:
+        path_to_result = get_path_to_result(original_image, result_image)
+
+    original_ratio = round(original_image.width / original_image.height, 2)
+    result_ratio = round(result_image.width / result_image.height, 2)
+
+    if original_ratio != result_ratio:
+        print("width and height ratio has changed")
+
+    try:
+        result_image.save(path_to_result)
+    except ValueError:
+        sys.exit('Incorrect path to save')
